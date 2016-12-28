90801562b20427269c664cde91959f53a2aa033d
ggoddx <ggoddx@umich.edu>
2016-12-28 16:23:02 -0500

    update engDupeTweets.py
    updated to allow specification of tweet text column

--- engDupeTweets.py
+++ engDupeTweets.py
@@ -4,8 +4,9 @@ import numpy as np
 
 def main():
     ## CSV file of Tweets and data
-    [fName] = getSysArgs.usage(['engDupeTweets.py',
-                                '<tweets_CSV_file_path>'])[1:]
+    [fName, twCol] = getSysArgs.usage(['engDupeTweets.py',
+                                       '<tweets_CSV_file_path>',
+                                       '<tweet_text_col_name>'])[1:]
 
     ## Open CSV file
     dataCSV = list(csv.reader(open(fName, 'rU')))
@@ -26,7 +27,7 @@ def main():
 
     for line in dataEng:
         ## Index of column named Text
-        textCol = colNames.index('Text')
+        textCol = colNames.index(twCol)
 
         ## Tweet text
         tweet = line[textCol]
