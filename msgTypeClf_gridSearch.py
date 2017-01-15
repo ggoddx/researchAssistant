f1576ed8f2d3ad2240dd8c423a0e0bfdd7d6a205
ggoddx <ggoddx@umich.edu>
2017-01-14 19:51:13 -0500

    update msgTypeClf_gridSearch.py
    updated to handle vectorizer and kernel options in addition to maximum and minimum document frequencies

--- msgTypeClf_gridSearch.py
+++ msgTypeClf_gridSearch.py
@@ -1,5 +1,5 @@
 from sklearn import svm
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.metrics import confusion_matrix, f1_score
 from sklearn.model_selection import StratifiedKFold
 
@@ -8,14 +8,12 @@ import numpy as np
 
 
 def main():
-    np.random.seed(0)
-
     ## Number of folds for cross-validation
     N_FOLDS = 5
 
     ## Training data for classification, observation column name, and label
     #  column name
-    [fName, obsCol, lblCol] = getSysArgs.usage(['msgTypeClf.py',
+    [fName, obsCol, lblCol] = getSysArgs.usage(['msgTypeClf_gridSearch.py',
                                                 '<train_data_filepath>',
                                                 '<observation_column_name>',
                                                 '<label_column_name>'])[1:]
@@ -27,6 +25,17 @@ def main():
     colNames = train[0]
 
     train = np.array(train[1:])
+
+    ## Open random sequence file (courtesy of random.org)
+    seqFile = open('trainRandSeq.txt', 'rU')
+
+    ## List form of permutation in file
+    permu = []
+
+    for line in seqFile:
+        permu.append(int(line.strip()))
+
+    train = train[permu]
 
     ## Tweet text before removing html entities
     tweetTxtTemp = train[:, colNames.index(obsCol)]
@@ -51,68 +60,102 @@ def main():
     ## Labels
     labels = train[:, colNames.index(lblCol)]
 
-    ## Support vector machines classifier
-    clf = svm.SVC(kernel = 'linear')
-
     ## 5-fold stratified cross-validation
-    skf5 = StratifiedKFold(n_splits = N_FOLDS, shuffle = True)
+    skf5 = StratifiedKFold(n_splits = N_FOLDS)
 
-    ## Grid for maximum and minimum document frequencies
-    dfGrid = [0.001, 0.01, 0.02, 0.05, 0.1]
+    ## Grid for vectorizer
+    txt2vectGrid = ['binary', 'count', 'tf', 'tfidf']
 
-    for maxDF in dfGrid:
-        for minDF in dfGrid:
-            print 'max-df', 1 - maxDF, '| min-df', minDF
+    ## Grid for maximum document frequencies
+    maxDFgrid = [0.99, 1.0]
 
-            ## Text vectorizer
-            txt2vect = CountVectorizer(max_df = 0.99, min_df = 0.01)
+    ## Grid for minimum document frequencies
+    minDFgrid = [0.01, 1]
 
-            ## Training features
-            feats = txt2vect.fit_transform(tweetTxt)
+    ## Grid for kernels
+    kernGrid = ['rbf', 'linear', 'poly', 'sigmoid']
 
-            ## Data split into folds
-            folds = skf5.split(feats, labels)
+    ## F1-scores from each fold for each grid result
+    gridResults = [['vectorizer', 'max-df', 'min-df', 'kernel', 'F1-score_1',
+                    'F1-score_2', 'F1-score_3', 'F1-score_4', 'F1-score_5']]
 
-            ## F1-scores from cross-validation
-            cvF1 = []
+    for vectzr in txt2vectGrid:
+        for maxDF in maxDFgrid:
+            for minDF in minDFgrid:
+                ## Text vectorizer
+                txt2vect = None
 
-            ## Confusion matricies from cross-validation
-            cvCM = []
+                if vectzr == 'binary':
+                    txt2vect = CountVectorizer(binary = True, max_df = maxDF,
+                                               min_df = minDF)
+                elif vectzr == 'count':
+                    txt2vect = CountVectorizer(binary = False, max_df = maxDF,
+                                               min_df = minDF)
+                elif vectzr == 'tf':
+                    txt2vect = TfidfVectorizer(max_df = maxDF, min_df = minDF,
+                                               use_idf = False)
+                elif vectzr == 'tfidf':
+                    txt2vect = TfidfVectorizer(max_df = maxDF, min_df = minDF,
+                                               use_idf = True)
 
-            ## Counter for fold iterations
-            fold = 1
+                ## Training features
+                feats = txt2vect.fit_transform(tweetTxt)
 
-            for trainIndices, testIndices in folds:
-                clf.fit(feats[trainIndices], labels[trainIndices])
+                for kern in kernGrid:
+                    print 'vectorizer:', vectzr, '| max-df:', maxDF, '| min-df:', minDF, '| kernel:', kern
 
-                ## True labels
-                trueLabels = labels[testIndices]
+                    ## Support vector machines classifier
+                    clf = svm.SVC(kernel = kern)
 
-                ## Predicted labels
-                predLabels = clf.predict(feats[testIndices])
+                    ## Data split into folds
+                    folds = skf5.split(feats, labels)
 
-                cvF1.append(f1_score(trueLabels, predLabels,
-                                     average = 'micro'))
+                    ## F1-scores from cross-validation
+                    cvF1 = []
 
-                cvCM.append(confusion_matrix(trueLabels, predLabels, labels = [
-                            'Achievement', 'Address', 'Appeal',
-                            'Confrontation', 'Endorsement', 'Generic',
-                            'Regards', 'Update']))
+                    ## Confusion matricies from cross-validation
+                    cvCM = []
 
-                print 'Fold', fold, 'of', N_FOLDS, 'complete'
-                fold += 1
+                    ## Counter for fold iterations
+                    fold = 1
 
-            cvF1 = np.array(cvF1)
-            print cvF1
-            print 'mean: ', np.mean(cvF1)
-            print 'min: ', np.min(cvF1)
-            print 'median: ', np.median(cvF1)
-            print 'max: ', np.max(cvF1)
-            print 'mean confusion matrix'
-            print np.mean(np.array(cvCM), axis = 0)
+                    for trainIndices, testIndices in folds:
+                        clf.fit(feats[trainIndices], labels[trainIndices])
 
-            for cm in cvCM:
-                print cm
+                        ## True labels
+                        trueLabels = labels[testIndices]
+
+                        ## Predicted labels
+                        predLabels = clf.predict(feats[testIndices])
+
+                        cvF1.append(f1_score(trueLabels, predLabels,
+                                             average = 'micro'))
+
+                        cvCM.append(confusion_matrix(trueLabels, predLabels, labels = ['Achievement', 'Address', 'Appeal', 'Confrontation', 'Endorsement', 'Generic', 'Regards', 'Update']))
+
+                        print 'Fold', fold, 'of', N_FOLDS, 'complete'
+                        fold += 1
+
+                    gridResults.append([vectzr, maxDF, minDF, kern] + cvF1)
+
+                    cvF1 = np.array(cvF1)
+                    print cvF1
+                    print 'mean: ', np.mean(cvF1)
+                    print 'min: ', np.min(cvF1)
+                    print 'median: ', np.median(cvF1)
+                    print 'max: ', np.max(cvF1)
+                    print 'mean confusion matrix'
+                    print np.mean(np.array(cvCM), axis = 0)
+
+                    for cm in cvCM:
+                        print cm
+
+    print np.array(gridResults)
+
+    ## File to write results of grid search
+    resCSV = csv.writer(open('gridResults.csv', 'wb', buffering = 0))
+
+    resCSV.writerows(gridResults)
 
     return
 
