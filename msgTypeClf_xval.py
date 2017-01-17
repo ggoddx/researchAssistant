4a4f29f4a58b365da996fd8c08af72b817a57c04
ggoddx <ggoddx@umich.edu>
2017-01-17 15:21:30 -0500

    update msgTypeClf_xval.py
    refactored to leverage the printCV function

--- msgTypeClf_xval.py
+++ msgTypeClf_xval.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import StratifiedKFold
 
 import csv, getSysArgs
 import numpy as np
+import results
 
 
 def main():
@@ -33,50 +34,7 @@ def main():
                                   'Generic': 1, 'Regards': 1, 'Update': 1},
                   kernel = 'linear')
 
-    ## 5-fold stratified cross-validation
-    skf5 = StratifiedKFold(n_splits = N_FOLDS)
-
-    ## Data split into folds
-    folds = skf5.split(feats, train.labels)
-
-    ## F1-scores from cross-validation
-    cvF1 = []
-
-    ## Confusion matricies from cross-validation
-    cvCM = []
-
-    ## Counter for fold iterations
-    fold = 1
-
-    for trainIndices, testIndices in folds:
-        clf.fit(feats[trainIndices], train.labels[trainIndices])
-
-        ## True labels
-        trueLabels = train.labels[testIndices]
-
-        ## Predicted labels
-        predLabels = clf.predict(feats[testIndices])
-
-        cvF1.append(f1_score(trueLabels, predLabels, average = 'micro'))
-
-        cvCM.append(confusion_matrix(trueLabels, predLabels, labels = [
-                    'Achievement', 'Address', 'Appeal', 'Confrontation',
-                    'Endorsement', 'Generic', 'Regards', 'Update']))
-
-        print 'Fold', fold, 'of', N_FOLDS, 'complete'
-        fold += 1
-
-    cvF1 = np.array(cvF1)
-    print cvF1
-    print 'mean: ', np.mean(cvF1)
-    print 'min: ', np.min(cvF1)
-    print 'median: ', np.median(cvF1)
-    print 'max: ', np.max(cvF1)
-    print 'mean confusion matrix'
-    print np.mean(np.array(cvCM), axis = 0)
-
-    for cm in cvCM:
-        print cm
+    results.printCV(N_FOLDS, feats, train.labels, clf)
 
     return
 
