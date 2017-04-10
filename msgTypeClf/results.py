041e40024cc51fb81bb1fa296b1651ee59c495aa
ggoddx <ggoddx@umich.edu>
2017-04-09 23:24:10 -0400

    update results.py
    added ability to store and return each tweet's prediction

--- msgTypeClf/results.py
+++ msgTypeClf/results.py
@@ -13,8 +13,8 @@ import numpy as np
 #  @param feats numpy array
 #   text-feature matrix of tweets
 #
-#  @param labels numpy array
-#   label for each tweet
+#  @param train ClfData object
+#   contains labels, original indices, and tweet text from training data
 #
 #  @param clf sci-kit learn classifier object
 #   classification model
@@ -26,13 +26,15 @@ import numpy as np
 #  @param gridParams list
 #   parameters involved in the point of the grid search
 #   (for use with prininting results during a grid search)
-def printCV(n_folds, feats, labels, clf, gridResults = None,
-            gridParams = None):
+#
+#  @returns predictions list
+#   list of lists containing predicted labels for each tweet
+def printCV(n_folds, feats, train, clf, gridResults = None, gridParams = None):
     ## 5-fold stratified cross-validation
     skf5 = StratifiedKFold(n_splits = n_folds)
 
     ## Data split into folds
-    folds = skf5.split(feats, labels)
+    folds = skf5.split(feats, train.labels)
 
     ## List of labels
     lbls = ['Achievement', 'Address', 'Appeal', 'Confrontation', 'Endorsement',
@@ -47,20 +49,33 @@ def printCV(n_folds, feats, labels, clf, gridResults = None,
     ## Label-level results from cross-validation
     cvLblRes = []
 
+    ## To store predictions
+    predictions = [['OrigIndex', 'Text', 'True Message Type',
+                    'Predicted Message Type']]
+
     ## Counter for fold iterations
     fold = 1
 
     for trainIndices, testIndices in folds:
-        clf.fit(feats[trainIndices], labels[trainIndices])
+        ## Original indices
+        origIs = train.indices[testIndices]
+
+        ## Tweet text
+        tweet = train.tweetTxt[testIndices]
 
         ## True labels
-        trueLabels = labels[testIndices]
+        trueLabels = train.labels[testIndices]
+
+        clf.fit(feats[trainIndices], train.labels[trainIndices])
 
         ## Predicted labels
         predLabels = clf.predict(feats[testIndices])
 
-        cvF1.append(f1_score(trueLabels, predLabels, average = 'micro'))
+        for i in range(origIs.shape[0]):
+            predictions.append([origIs[i], tweet[i], trueLabels[i],
+                                predLabels[i]])
 
+        cvF1.append(f1_score(trueLabels, predLabels, average = 'micro'))
         cvCM.append(confusion_matrix(trueLabels, predLabels, labels = lbls))
 
         cvLblRes.append(np.transpose(np.array(prfs(trueLabels, predLabels,
@@ -86,4 +101,4 @@ def printCV(n_folds, feats, labels, clf, gridResults = None,
     for cm in cvCM:
         print cm
 
-    return
+    return predictions
